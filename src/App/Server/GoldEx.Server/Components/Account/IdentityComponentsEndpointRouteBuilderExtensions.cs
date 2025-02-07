bfbdd72d0ac1933c04ef916ec212254668858a2f
Masoud Khodadadi <masoud.xpress@gmail.com>
2025-02-08 01:38:10 +0330

    Add Google login support and refactor auth processes

--- src/App/Server/GoldEx.Server/Components/Account/IdentityComponentsEndpointRouteBuilderExtensions.cs
+++ src/App/Server/GoldEx.Server/Components/Account/IdentityComponentsEndpointRouteBuilderExtensions.cs
@@ -1,8 +1,13 @@
 using System.Security.Claims;
 using GoldEx.Sdk.Server.Domain.Entities.Identity;
+using GoldEx.Server.Components.Account.Pages;
+using GoldEx.Server.Components.Account.Pages.Manage;
 using GoldEx.Server.Components.Account.Pages.ViewModels;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http.Extensions;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Primitives;
 
 namespace GoldEx.Server.Components.Account;
 
@@ -29,6 +34,44 @@ internal static class IdentityComponentsEndpointRouteBuilderExtensions
             var result = await signInManager.PasswordSignInAsync(model.Username, model.Password, model.RememberMe, lockoutOnFailure: true);
 
             return result.Succeeded ? Results.Ok() : Results.Forbid();
+        });
+
+        accountGroup.MapPost("/PerformExternalLogin", (
+            HttpContext context,
+            [FromServices] SignInManager<AppUser> signInManager,
+            [FromForm] string provider,
+            [FromForm] string returnUrl) =>
+        {
+            IEnumerable<KeyValuePair<string, StringValues>> query = [
+                new("ReturnUrl", returnUrl),
+                new("Action", ExternalLogin.LoginCallbackAction)];
+
+            var redirectUrl = UriHelper.BuildRelative(
+                context.Request.PathBase,
+                "/Account/ExternalLogin",
+                QueryString.Create(query));
+
+            var properties = signInManager.ConfigureExternalAuthenticationProperties(provider, redirectUrl);
+            return TypedResults.Challenge(properties, [provider]);
+        });
+
+        var manageGroup = accountGroup.MapGroup("/Manage").RequireAuthorization();
+
+        manageGroup.MapPost("/LinkExternalLogin", async (
+            HttpContext context,
+            [FromServices] SignInManager<AppUser> signInManager,
+            [FromForm] string provider) =>
+        {
+            // Clear the existing external cookie to ensure a clean login process
+            await context.SignOutAsync(IdentityConstants.ExternalScheme);
+
+            var redirectUrl = UriHelper.BuildRelative(
+                context.Request.PathBase,
+                "/Account/Manage/ExternalLogins",
+                QueryString.Create("Action", ExternalLogins.LinkLoginCallbackAction));
+
+            var properties = signInManager.ConfigureExternalAuthenticationProperties(provider, redirectUrl, signInManager.UserManager.GetUserId(context.User));
+            return TypedResults.Challenge(properties, [provider]);
         });
 
         return accountGroup;
