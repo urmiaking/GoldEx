b55ada3b0e76f6e7ef3eb32759b54e1f46fdd9cc
Masoud - Company <masoud.xpress@gmail.com>
2025-02-08 23:13:02 +0330

    Update project structure, routing, and UI components

---
+++ src/App/Client/GoldEx.Client.Components/Extensions/IdentityExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Security.Claims;
+
+namespace GoldEx.Client.Components.Extensions;
+
+public static class IdentityExtensions
+{
+    public static string GetDisplayName(this ClaimsPrincipal principal)
+    {
+        var firstNameClaim = principal.FindFirst(ClaimTypes.GivenName);
+        var lastNameClaim = principal.FindFirst(ClaimTypes.Surname);
+
+        if (firstNameClaim == null && lastNameClaim == null)
+            return principal.Identity?.Name ?? "N/A";
+
+        return $"{firstNameClaim?.Value} {lastNameClaim?.Value}".Trim();
+    }
+}
