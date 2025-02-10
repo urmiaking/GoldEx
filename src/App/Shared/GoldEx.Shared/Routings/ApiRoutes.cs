9ae3ed0a2ed8a9fe80a231df1bf7f0b31446252c
Masoud - Company <masoud.xpress@gmail.com>
2025-02-10 17:43:46 +0330

    Add API route constants and update URL generation

---
+++ src/App/Shared/GoldEx.Shared/Routings/ApiRoutes.cs
@@ -0,0 +1,21 @@
+﻿namespace GoldEx.Shared.Routings;
+
+public static class ApiRoutes
+{
+    public static class Account
+    {
+        public const string Base = "/Account";
+
+        public const string Login = "Login";
+        public const string Logout = "Logout";
+        public const string PerformExternalLogin = "PerformExternalLogin";
+
+        public static class Manage
+        {
+            public const string Base = "Manage";
+
+            public const string LinkExternalLogin = "LinkExternalLogin";
+
+        }
+    }
+}
---
+++ src/App/Shared/GoldEx.Shared/Routings/ApiUrls.cs
@@ -0,0 +1,19 @@
+﻿using GoldEx.Sdk.Common.Extensions;
+
+namespace GoldEx.Shared.Routings;
+
+public class ApiUrls
+{
+    private static string BuildUrl(params string[] segments)
+    {
+        return string.Join('/', segments);
+    }
+
+    public static class Account
+    {
+        public static string LinkExternalLogin() => BuildUrl(ApiRoutes.Account.Base, ApiRoutes.Account.Manage.Base, ApiRoutes.Account.Manage.LinkExternalLogin);
+
+        public static string Logout(string? returnUrl) => BuildUrl(ApiRoutes.Account.Base, ApiRoutes.Account.Logout)
+            .AppendQueryString(new { returnUrl });
+    }
+}
