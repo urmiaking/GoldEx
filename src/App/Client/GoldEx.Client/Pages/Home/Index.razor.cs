20dc14304308d67379ba509acbb0bd609b8cfb2b
Masoud - Company <masoud.xpress@gmail.com>
2025-02-24 08:14:20 +0330

    Add breadcrumb

---
+++ src/App/Client/GoldEx.Client/Pages/Home/Index.razor.cs
@@ -0,0 +1,12 @@
+﻿using GoldEx.Shared.Routings;
+using MudBlazor;
+
+namespace GoldEx.Client.Pages.Home;
+
+public partial class Index
+{
+    private readonly List<BreadcrumbItem> _breadcrumbs =
+    [
+        new BreadcrumbItem("صفحه اصلی", href: ClientRoutes.Home.Index, icon: Icons.Material.Filled.Home)
+    ];
+}
