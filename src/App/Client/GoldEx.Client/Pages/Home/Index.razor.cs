2cf199809957b47b9a650f3db89dcbf235609e0c
Masoud - Company <masoud.xpress@gmail.com>
2025-02-24 21:34:01 +0330

    Add dynamic content loading to expansion panels

--- src/App/Client/GoldEx.Client/Pages/Home/Index.razor.cs
+++ src/App/Client/GoldEx.Client/Pages/Home/Index.razor.cs
@@ -1,4 +1,7 @@
-﻿using GoldEx.Shared.Routings;
+﻿using GoldEx.Client.Pages.Home.Components;
+using GoldEx.Client.Pages.Products.Components;
+using GoldEx.Shared.Routings;
+using Microsoft.AspNetCore.Components;
 using MudBlazor;
 
 namespace GoldEx.Client.Pages.Home;
@@ -9,4 +12,44 @@ public partial class Index
     [
         new BreadcrumbItem("صفحه اصلی", href: ClientRoutes.Home.Index, icon: Icons.Material.Filled.Home)
     ];
+    private RenderFragment? _productListContent;
+    private RenderFragment? _priceBoardContent;
+
+    private async Task OnProductsListExpandedChanged(bool newVal)
+    {
+        if (newVal)
+        {
+            await Task.Delay(50);
+            _productListContent = builder =>
+            {
+                builder.OpenComponent(0, typeof(ProductsList));
+                builder.CloseComponent();
+            };
+            StateHasChanged();
+        }
+        else
+        {
+            Task.Delay(350).ContinueWith(t => { _productListContent = null; StateHasChanged(); }).CatchAndLog();
+            StateHasChanged();
+        }
+    }
+
+    private async Task OnPriceBoardExpandedChanged(bool newVal)
+    {
+        if (newVal)
+        {
+            await Task.Delay(50);
+            _priceBoardContent = builder =>
+            {
+                builder.OpenComponent(0, typeof(PriceBoard));
+                builder.CloseComponent();
+            };
+            StateHasChanged();
+        }
+        else
+        {
+            Task.Delay(350).ContinueWith(t => { _priceBoardContent = null; StateHasChanged(); }).CatchAndLog();
+            StateHasChanged();
+        }
+    }
 }
