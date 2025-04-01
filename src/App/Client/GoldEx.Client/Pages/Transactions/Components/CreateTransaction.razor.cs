77b37bf187159ff47006de31b5fb484221fe9842
Masoud Khodadadi <masoud.xpress@gmail.com>
2025-04-01 15:47:50 +0330

    Add Create transaction page

---
+++ src/App/Client/GoldEx.Client/Pages/Transactions/Components/CreateTransaction.razor.cs
@@ -0,0 +1,8 @@
+﻿using GoldEx.Client.Pages.Transactions.ViewModels;
+
+namespace GoldEx.Client.Pages.Transactions.Components;
+
+public partial class CreateTransaction
+{
+    public CreateTransactionVm _model = new ();
+}
