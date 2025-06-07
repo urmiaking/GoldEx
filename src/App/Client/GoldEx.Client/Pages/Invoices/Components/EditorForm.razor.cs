1cc3bf44ce26dad970121a570d4c87e1b31e67d4
Masoud Khodadadi <masoud.xpress@gmail.com>
2025-06-07 23:41:11 +0330

    Add simple table for invoice items

--- src/App/Client/GoldEx.Client/Pages/Invoices/Components/EditorForm.razor.cs
+++ src/App/Client/GoldEx.Client/Pages/Invoices/Components/EditorForm.razor.cs
@@ -3,10 +3,15 @@ using GoldEx.Client.Pages.Customers.ViewModels;
 using GoldEx.Client.Pages.Invoices.Validators;
 using GoldEx.Client.Pages.Invoices.ViewModels;
 using GoldEx.Shared.DTOs.Customers;
+using GoldEx.Shared.DTOs.Prices;
 using GoldEx.Shared.DTOs.PriceUnits;
+using GoldEx.Shared.DTOs.Products;
+using GoldEx.Shared.DTOs.Settings;
+using GoldEx.Shared.Enums;
 using GoldEx.Shared.Services;
 using Microsoft.AspNetCore.Components;
 using MudBlazor;
+using System;
 
 namespace GoldEx.Client.Pages.Invoices.Components;
 
@@ -15,14 +20,34 @@ public partial class EditorForm
     [Parameter] public InvoiceVm Model { get; set; } = InvoiceVm.CreateDefaultInstance();
 
     private readonly InvoiceValidator _invoiceValidator = new();
+    private GetSettingResponse? _setting;
+    private GetPriceResponse? _gramPrice;
     private MudForm _form = default!;
     private List<GetPriceUnitTitleResponse> _priceUnits = [];
     private bool _isCustomerCreditLimitMenuOpen;
+    private string? _barcode;
+    private string? _barcodeFieldHelperText;
 
     protected override async Task OnParametersSetAsync()
     {
         await LoadPriceUnitsAsync();
+        await LoadSettingsAsync();
+        await LoadGramPrice();
         await base.OnParametersSetAsync();
+    }
+
+    private async Task LoadGramPrice()
+    {
+        await SendRequestAsync<IPriceService, GetPriceResponse?>(
+            action: (s, ct) => s.GetAsync(UnitType.Gold18K, ct),
+            afterSend: response => _gramPrice = response);
+    }
+
+    private async Task LoadSettingsAsync()
+    {
+        await SendRequestAsync<ISettingService, GetSettingResponse?>(
+            action: (s, ct) => s.GetAsync(ct),
+            afterSend: response => _setting = response);
     }
 
     private async Task LoadPriceUnitsAsync()
@@ -69,5 +94,38 @@ public partial class EditorForm
                 Model.Customer = CustomerVm.CreateFrom(response);
                 OnCustomerCreditLimitChanged(response.CreditLimit);
             });
+    }
+
+    private async Task OnBarcodeChanged(string barcode)
+    {
+        _barcode = barcode;
+
+        if (string.IsNullOrWhiteSpace(barcode))
+        {
+            OnBarcodeCleared();
+            return;
+        }
+
+        await SendRequestAsync<IProductService, GetProductResponse?>(async (s, ct) => await s.GetAsync(barcode, ct),
+            response =>
+            {
+                if (response is null)
+                    return;
+
+                decimal.TryParse(_gramPrice?.Value, out var gramPrice);
+
+                Model.AddInvoiceItem(response,
+                    gramPrice,
+                    null,
+                    _setting?.TaxPercent,
+                    response.ProductType == ProductType.Gold
+                        ? _setting?.GoldProfitPercent
+                        : _setting?.JewelryProfitPercent);
+            });
+    }
+
+    private void OnBarcodeCleared()
+    {
+        _barcode = null;
     }
 }
