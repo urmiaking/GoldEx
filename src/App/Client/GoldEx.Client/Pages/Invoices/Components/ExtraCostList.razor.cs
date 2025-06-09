bab6f4668a6bb1d17cc41d9afc01cb5144d2c907
Masoud - Company <masoud.xpress@gmail.com>
2025-06-09 10:42:15 +0330

    Add extra cost manager for invoice

--- src/App/Client/GoldEx.Client/Pages/Invoices/Components/ExtraCostList.razor.cs
+++ src/App/Client/GoldEx.Client/Pages/Invoices/Components/ExtraCostList.razor.cs
@@ -1,6 +1,38 @@
-﻿namespace GoldEx.Client.Pages.Invoices.Components;
+﻿using GoldEx.Client.Pages.Invoices.ViewModels;
+using Microsoft.AspNetCore.Components;
+
+namespace GoldEx.Client.Pages.Invoices.Components;
 
 public partial class ExtraCostList
 {
-    
+    [Parameter] public List<InvoiceExtraCostVm> Items { get; set; } = [];
+
+    protected override void OnParametersSet()
+    {
+        if (!Items.Any())
+        {
+            Items.Add(new InvoiceExtraCostVm
+            {
+                Amount = 0,
+                Description = string.Empty
+            });
+        }
+
+        base.OnParametersSet();
+    }
+
+    private void AddItem()
+    {
+        Items.Add(new InvoiceExtraCostVm
+        {
+            Amount = 0,
+            Description = string.Empty
+        });
+    }
+
+    private void RemoveItem(InvoiceExtraCostVm item)
+    {
+        if (Items.Count > 1)
+            Items.Remove(item);
+    }
 }
--- src/App/Client/GoldEx.Client/Pages/Invoices/ViewModels/InvoiceExtraCostVm.cs
+++ src/App/Client/GoldEx.Client/Pages/Invoices/ViewModels/InvoiceExtraCostVm.cs
@@ -1,10 +1,14 @@
 ﻿using GoldEx.Shared.DTOs.PriceUnits;
+using System.ComponentModel.DataAnnotations;
 
 namespace GoldEx.Client.Pages.Invoices.ViewModels;
 
 public class InvoiceExtraCostVm
 {
+    [Display(Name = "مبلغ")]
     public decimal Amount { get; set; }
+
+    [Display(Name = "بابت")]
     public string? Description { get; set; }
     public GetPriceUnitTitleResponse? PriceUnit { get; set; }
 }
