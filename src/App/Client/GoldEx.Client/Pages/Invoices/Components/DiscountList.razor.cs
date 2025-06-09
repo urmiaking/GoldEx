71b22a2962e577c8e82fed403e59da20e041baec
Masoud - Company <masoud.xpress@gmail.com>
2025-06-09 10:31:04 +0330

    Add discount manager for invoice

---
+++ src/App/Client/GoldEx.Client/Pages/Invoices/Components/DiscountList.razor.cs
@@ -0,0 +1,38 @@
+﻿using GoldEx.Client.Pages.Invoices.ViewModels;
+using Microsoft.AspNetCore.Components;
+
+namespace GoldEx.Client.Pages.Invoices.Components;
+
+public partial class DiscountList
+{
+    [Parameter] public List<InvoiceDiscountVm> Items { get; set; } = [];
+
+    protected override void OnParametersSet()
+    {
+        if (!Items.Any())
+        {
+            Items.Add(new InvoiceDiscountVm
+            {
+                Amount = 0,
+                Description = string.Empty
+            });
+        }
+
+        base.OnParametersSet();
+    }
+
+    private void AddItem()
+    {
+        Items.Add(new InvoiceDiscountVm
+        {
+            Amount = 0,
+            Description = string.Empty
+        });
+    }
+
+    private void RemoveItem(InvoiceDiscountVm item)
+    {
+        if (Items.Count > 1) 
+            Items.Remove(item);
+    }
+}
--- src/App/Client/GoldEx.Client/Pages/Invoices/ViewModels/InvoiceDiscountVm.cs
+++ src/App/Client/GoldEx.Client/Pages/Invoices/ViewModels/InvoiceDiscountVm.cs
@@ -1,10 +1,14 @@
-﻿using GoldEx.Shared.DTOs.PriceUnits;
+﻿using System.ComponentModel.DataAnnotations;
+using GoldEx.Shared.DTOs.PriceUnits;
 
 namespace GoldEx.Client.Pages.Invoices.ViewModels;
 
 public class InvoiceDiscountVm
 {
+    [Display(Name = "مبلغ")]
     public decimal Amount { get;  set; }
+
+    [Display(Name = "بابت")]
     public string? Description { get; set; }
     public GetPriceUnitTitleResponse? PriceUnit { get; set; }
 }
