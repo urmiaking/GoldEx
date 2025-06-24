f85c59feeb5038d2844d1980f1ccf6875a6d3613
Masoud Khodadadi <masoud.xpress@gmail.com>
2025-06-24 11:58:17 +0330

    Add report layout specifications

---
+++ src/App/Server/GoldEx.Server.Infrastructure/Specifications/ReportLayouts/ReportLayoutsByNameSpecification.cs
@@ -0,0 +1,12 @@
+﻿using GoldEx.Sdk.Server.Infrastructure.Specifications;
+using GoldEx.Server.Domain.ReportLayoutAggregate;
+
+namespace GoldEx.Server.Infrastructure.Specifications.ReportLayouts;
+
+public class ReportLayoutsByNameSpecification : SpecificationBase<ReportLayout>
+{
+    public ReportLayoutsByNameSpecification(string name)
+    {
+        AddCriteria(x => x.Name == name);
+    }
+}
