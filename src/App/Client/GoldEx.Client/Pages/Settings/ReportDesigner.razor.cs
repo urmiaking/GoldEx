59287e21f46e67cbd60de25a5b3bc51e7bbdffab
Masoud Khodadadi <masoud.xpress@gmail.com>
2025-06-26 22:49:31 +0330

    Added designer page

---
+++ src/App/Client/GoldEx.Client/Pages/Settings/ReportDesigner.razor.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace GoldEx.Client.Pages.Settings;
+
+public partial class ReportDesigner
+{
+    [Parameter] public string Name { get; set; } = default!;
+
+    [Parameter, SupplyParameterFromQuery] public string? DisplayName { get; set; }
+}
