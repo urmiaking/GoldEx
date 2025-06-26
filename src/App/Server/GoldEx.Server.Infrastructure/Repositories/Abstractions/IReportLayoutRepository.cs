da7b9d1662a09cf9b0b083a60a7ef36197ee25b8
Masoud - Company <masoud.xpress@gmail.com>
2025-06-26 16:19:39 +0330

    Remove report layout from db

--- src/App/Server/GoldEx.Server.Infrastructure/Repositories/Abstractions/IReportLayoutRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using GoldEx.Sdk.Server.Infrastructure.Repositories;
-using GoldEx.Server.Domain.ReportLayoutAggregate;
-
-namespace GoldEx.Server.Infrastructure.Repositories.Abstractions;
-
-public interface IReportLayoutRepository : IRepository<ReportLayout>,
-    ICreateRepository<ReportLayout>,
-    IUpdateRepository<ReportLayout>,
-    IDeleteRepository<ReportLayout>;
