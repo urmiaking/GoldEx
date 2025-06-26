2891a190e8b9f760a5e67fc532031a00d36f288a
Masoud - Company <masoud.xpress@gmail.com>
2025-06-26 21:07:56 +0330

    Add report list provider

---
+++ src/App/Server/GoldEx.Server.Application/Services/ReportService.cs
@@ -0,0 +1,20 @@
+﻿using GoldEx.Sdk.Common.DependencyInjections;
+using GoldEx.Server.Application.Reporting;
+using GoldEx.Shared.DTOs.Reports;
+using GoldEx.Shared.Services;
+
+namespace GoldEx.Server.Application.Services;
+
+[ScopedService]
+internal class ReportService(ReportStorageExtension reportStorage) : IReportService
+{
+    public Task<List<GetReportResponse>> GetListAsync(CancellationToken cancellationToken = default)
+    {
+        var reportUrls = reportStorage.GetUrls();
+
+        var response = reportUrls.Select(kvp => 
+            new GetReportResponse(kvp.Key, kvp.Value));
+
+        return Task.FromResult(response.ToList());
+    }
+}
