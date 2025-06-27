75007de389d78aa1c7119a9313e71c823ec1c050
Masoud Khodadadi <masoud.xpress@gmail.com>
2025-06-27 18:35:41 +0330

    Add report parameters

--- src/App/Server/GoldEx.Server/Reports/InvoiceReport.Designer.cs
+++ src/App/Server/GoldEx.Server/Reports/InvoiceReport.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            DevExpress.XtraPrinting.BarCode.Code39Generator code39Generator1 = new DevExpress.XtraPrinting.BarCode.Code39Generator();
             DevExpress.DataAccess.ObjectBinding.ObjectConstructorInfo objectConstructorInfo1 = new DevExpress.DataAccess.ObjectBinding.ObjectConstructorInfo();
             DevExpress.DataAccess.ObjectBinding.Parameter parameter1 = new DevExpress.DataAccess.ObjectBinding.Parameter();
             DevExpress.DataAccess.ObjectBinding.Parameter parameter2 = new DevExpress.DataAccess.ObjectBinding.Parameter();
@@ -38,6 +39,7 @@
             this.evenDetailStyle = new DevExpress.XtraReports.UI.XRControlStyle();
             this.oddDetailStyle = new DevExpress.XtraReports.UI.XRControlStyle();
             this.TopMargin = new DevExpress.XtraReports.UI.TopMarginBand();
+            this.xrBarCode1 = new DevExpress.XtraReports.UI.XRBarCode();
             this.xrLabel7 = new DevExpress.XtraReports.UI.XRLabel();
             this.xrLabel8 = new DevExpress.XtraReports.UI.XRLabel();
             this.xrLabel2 = new DevExpress.XtraReports.UI.XRLabel();
@@ -75,6 +77,7 @@
             // TopMargin
             // 
             this.TopMargin.Controls.AddRange(new DevExpress.XtraReports.UI.XRControl[] {
+            this.xrBarCode1,
             this.xrLabel7,
             this.xrLabel8,
             this.xrLabel2,
@@ -87,10 +90,22 @@
             this.TopMargin.HeightF = 244.7916F;
             this.TopMargin.Name = "TopMargin";
             // 
+            // xrBarCode1
+            // 
+            this.xrBarCode1.AutoModule = true;
+            this.xrBarCode1.ExpressionBindings.AddRange(new DevExpress.XtraReports.UI.ExpressionBinding[] {
+            new DevExpress.XtraReports.UI.ExpressionBinding("BeforePrint", "Text", "[Invoice].[InvoiceNumber]")});
+            this.xrBarCode1.LocationFloat = new DevExpress.Utils.PointFloat(60.41669F, 21.16661F);
+            this.xrBarCode1.Name = "xrBarCode1";
+            this.xrBarCode1.Padding = new DevExpress.XtraPrinting.PaddingInfo(10, 10, 0, 0, 100F);
+            this.xrBarCode1.SizeF = new System.Drawing.SizeF(125F, 75F);
+            code39Generator1.WideNarrowRatio = 3F;
+            this.xrBarCode1.Symbology = code39Generator1;
+            // 
             // xrLabel7
             // 
             this.xrLabel7.Font = new DevExpress.Drawing.DXFont("B Nazanin", 12F, DevExpress.Drawing.DXFontStyle.Bold);
-            this.xrLabel7.LocationFloat = new DevExpress.Utils.PointFloat(10F, 166.625F);
+            this.xrLabel7.LocationFloat = new DevExpress.Utils.PointFloat(9.999998F, 143.7084F);
             this.xrLabel7.Multiline = true;
             this.xrLabel7.Name = "xrLabel7";
             this.xrLabel7.Padding = new DevExpress.XtraPrinting.PaddingInfo(2, 2, 0, 0, 100F);
@@ -100,14 +115,16 @@
             // 
             // xrLabel8
             // 
+            this.xrLabel8.AutoWidth = true;
+            this.xrLabel8.CanGrow = false;
             this.xrLabel8.ExpressionBindings.AddRange(new DevExpress.XtraReports.UI.ExpressionBinding[] {
-            new DevExpress.XtraReports.UI.ExpressionBinding("BeforePrint", "Text", "[Response].[DueDate]")});
+            new DevExpress.XtraReports.UI.ExpressionBinding("BeforePrint", "Text", "[Invoice].[DueDate]")});
             this.xrLabel8.Font = new DevExpress.Drawing.DXFont("B Nazanin", 12F, DevExpress.Drawing.DXFontStyle.Bold);
-            this.xrLabel8.LocationFloat = new DevExpress.Utils.PointFloat(116.6667F, 166.625F);
+            this.xrLabel8.LocationFloat = new DevExpress.Utils.PointFloat(116.6667F, 143.7084F);
             this.xrLabel8.Multiline = true;
             this.xrLabel8.Name = "xrLabel8";
             this.xrLabel8.Padding = new DevExpress.XtraPrinting.PaddingInfo(2, 2, 0, 0, 100F);
-            this.xrLabel8.SizeF = new System.Drawing.SizeF(244.7914F, 22.99998F);
+            this.xrLabel8.SizeF = new System.Drawing.SizeF(139.5831F, 22.99997F);
             this.xrLabel8.StylePriority.UseFont = false;
             this.xrLabel8.Text = "شماره فاکتور: ";
             this.xrLabel8.TextFormatString = "{0:yyyy-MM-dd}";
@@ -115,7 +132,7 @@
             // xrLabel2
             // 
             this.xrLabel2.Font = new DevExpress.Drawing.DXFont("B Nazanin", 12F, DevExpress.Drawing.DXFontStyle.Bold);
-            this.xrLabel2.LocationFloat = new DevExpress.Utils.PointFloat(10F, 130.1667F);
+            this.xrLabel2.LocationFloat = new DevExpress.Utils.PointFloat(9.999998F, 119.1666F);
             this.xrLabel2.Multiline = true;
             this.xrLabel2.Name = "xrLabel2";
             this.xrLabel2.Padding = new DevExpress.XtraPrinting.PaddingInfo(2, 2, 0, 0, 100F);
@@ -125,33 +142,39 @@
             // 
             // xrLabel6
             // 
+            this.xrLabel6.AutoWidth = true;
+            this.xrLabel6.CanGrow = false;
             this.xrLabel6.ExpressionBindings.AddRange(new DevExpress.XtraReports.UI.ExpressionBinding[] {
-            new DevExpress.XtraReports.UI.ExpressionBinding("BeforePrint", "Text", "[Response].[InvoiceDate]")});
+            new DevExpress.XtraReports.UI.ExpressionBinding("BeforePrint", "Text", "[Invoice].[InvoiceDate]")});
             this.xrLabel6.Font = new DevExpress.Drawing.DXFont("B Nazanin", 12F, DevExpress.Drawing.DXFontStyle.Bold);
-            this.xrLabel6.LocationFloat = new DevExpress.Utils.PointFloat(116.6667F, 130.1667F);
+            this.xrLabel6.LocationFloat = new DevExpress.Utils.PointFloat(116.6667F, 119.1666F);
             this.xrLabel6.Multiline = true;
             this.xrLabel6.Name = "xrLabel6";
             this.xrLabel6.Padding = new DevExpress.XtraPrinting.PaddingInfo(2, 2, 0, 0, 100F);
-            this.xrLabel6.SizeF = new System.Drawing.SizeF(244.7914F, 22.99998F);
+            this.xrLabel6.SizeF = new System.Drawing.SizeF(139.5831F, 22.99998F);
             this.xrLabel6.StylePriority.UseFont = false;
             this.xrLabel6.Text = "تاریخ فاکتور:";
             this.xrLabel6.TextFormatString = "{0:yyyy-MM-dd}";
             // 
             // xrLabel5
             // 
+            this.xrLabel5.AutoWidth = true;
+            this.xrLabel5.CanGrow = false;
             this.xrLabel5.ExpressionBindings.AddRange(new DevExpress.XtraReports.UI.ExpressionBinding[] {
             new DevExpress.XtraReports.UI.ExpressionBinding("BeforePrint", "Text", "[Setting].[InstitutionName]")});
-            this.xrLabel5.LocationFloat = new DevExpress.Utils.PointFloat(511.4582F, 130.1667F);
+            this.xrLabel5.Font = new DevExpress.Drawing.DXFont("B Titr", 26F);
+            this.xrLabel5.LocationFloat = new DevExpress.Utils.PointFloat(524.9999F, 130.1667F);
             this.xrLabel5.Multiline = true;
             this.xrLabel5.Name = "xrLabel5";
             this.xrLabel5.Padding = new DevExpress.XtraPrinting.PaddingInfo(2, 2, 0, 0, 100F);
-            this.xrLabel5.SizeF = new System.Drawing.SizeF(228.5418F, 23F);
+            this.xrLabel5.SizeF = new System.Drawing.SizeF(215.0001F, 57.375F);
+            this.xrLabel5.StylePriority.UseFont = false;
             this.xrLabel5.Text = "xrLabel5";
             // 
             // logoBox
             // 
             this.logoBox.ImageUrl = "uploads\\icons\\app\\logo.png";
-            this.logoBox.LocationFloat = new DevExpress.Utils.PointFloat(529.1666F, 19.1666F);
+            this.logoBox.LocationFloat = new DevExpress.Utils.PointFloat(592.0834F, 30.16669F);
             this.logoBox.Name = "logoBox";
             this.logoBox.SizeF = new System.Drawing.SizeF(100F, 100F);
             this.logoBox.Sizing = DevExpress.XtraPrinting.ImageSizeMode.ZoomImage;
@@ -165,29 +188,33 @@
             this.xrLabel4.Padding = new DevExpress.XtraPrinting.PaddingInfo(2, 2, 0, 0, 100F);
             this.xrLabel4.SizeF = new System.Drawing.SizeF(100F, 23F);
             this.xrLabel4.StylePriority.UseFont = false;
+            this.xrLabel4.StylePriority.UseTextAlignment = false;
             this.xrLabel4.Text = "شماره فاکتور:";
+            this.xrLabel4.TextAlignment = DevExpress.XtraPrinting.TextAlignment.TopLeft;
             // 
             // xrLabel3
             // 
+            this.xrLabel3.AutoWidth = true;
+            this.xrLabel3.CanGrow = false;
             this.xrLabel3.ExpressionBindings.AddRange(new DevExpress.XtraReports.UI.ExpressionBinding[] {
-            new DevExpress.XtraReports.UI.ExpressionBinding("BeforePrint", "Text", "[Response].[InvoiceNumber]")});
+            new DevExpress.XtraReports.UI.ExpressionBinding("BeforePrint", "Text", "[Invoice].[InvoiceNumber]")});
             this.xrLabel3.Font = new DevExpress.Drawing.DXFont("B Nazanin", 12F, DevExpress.Drawing.DXFontStyle.Bold);
             this.xrLabel3.LocationFloat = new DevExpress.Utils.PointFloat(116.6667F, 96.16661F);
             this.xrLabel3.Multiline = true;
             this.xrLabel3.Name = "xrLabel3";
             this.xrLabel3.Padding = new DevExpress.XtraPrinting.PaddingInfo(2, 2, 0, 0, 100F);
-            this.xrLabel3.SizeF = new System.Drawing.SizeF(244.7914F, 22.99998F);
+            this.xrLabel3.SizeF = new System.Drawing.SizeF(139.5831F, 22.99998F);
             this.xrLabel3.StylePriority.UseFont = false;
             this.xrLabel3.Text = "شماره فاکتور: ";
             // 
             // xrLabel1
             // 
             this.xrLabel1.Font = new DevExpress.Drawing.DXFont("B Nazanin", 24F, DevExpress.Drawing.DXFontStyle.Bold);
-            this.xrLabel1.LocationFloat = new DevExpress.Utils.PointFloat(10F, 10.00001F);
+            this.xrLabel1.LocationFloat = new DevExpress.Utils.PointFloat(294.375F, 10.00001F);
             this.xrLabel1.Multiline = true;
             this.xrLabel1.Name = "xrLabel1";
             this.xrLabel1.Padding = new DevExpress.XtraPrinting.PaddingInfo(2, 2, 0, 0, 100F);
-            this.xrLabel1.SizeF = new System.Drawing.SizeF(286.4582F, 67.79166F);
+            this.xrLabel1.SizeF = new System.Drawing.SizeF(154.1665F, 67.79166F);
             this.xrLabel1.StylePriority.UseFont = false;
             this.xrLabel1.Text = "فاکتور فروش";
             // 
@@ -209,7 +236,7 @@
             // 
             // objectDataSource2
             // 
-            parameter1.Name = "Response";
+            parameter1.Name = "Invoice";
             parameter1.Type = typeof(global::GoldEx.Shared.DTOs.Invoices.GetInvoiceResponse);
             parameter2.Name = "Setting";
             parameter2.Type = typeof(global::GoldEx.Shared.DTOs.Settings.GetSettingResponse);
@@ -269,5 +296,6 @@
         private DevExpress.XtraReports.UI.XRLabel xrLabel6;
         private DevExpress.XtraReports.UI.XRLabel xrLabel7;
         private DevExpress.XtraReports.UI.XRLabel xrLabel8;
+        private DevExpress.XtraReports.UI.XRBarCode xrBarCode1;
     }
 }
