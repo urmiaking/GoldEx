2d06c0e142454b0379885764360783ff8a348ef2
Masoud - Company <masoud.xpress@gmail.com>
2025-07-22 13:33:11 +0330

    Added style filter for invoice type

--- src/App/Shared/GoldEx.Shared/DTOs/Invoices/InvoiceFilter.cs
+++ src/App/Shared/GoldEx.Shared/DTOs/Invoices/InvoiceFilter.cs
@@ -2,4 +2,4 @@
 
 namespace GoldEx.Shared.DTOs.Invoices;
 
-public record InvoiceFilter(InvoicePaymentStatus? Status, DateTime? Start, DateTime? End);
+public record InvoiceFilter(InvoicePaymentStatus? Status, InvoiceType? InvoiceType, DateTime? Start, DateTime? End);
