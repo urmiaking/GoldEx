1a141d0dc15663972de5197c2082c29ea00d9cf7
Masoud Khodadadi <masoud.xpress@gmail.com>
2025-08-03 22:58:04 +0330

    Add invoice validation for reused payment vouchers
    Updated `InvoicePaymentDtoValidator` to include a check
    for payment vouchers already used in other invoices by
    introducing a new method and dependency on `IInvoiceRepository`.
    Modified validation rules for `VoucherId` accordingly.
    Adjusted `InvoiceRequestDtoValidator` to pass the new
    repository dependency.

---
+++ src/App/Server/GoldEx.Server.Infrastructure/Specifications/Invoices/InvoicesByVoucherIdSpecification.cs
@@ -0,0 +1,13 @@
+﻿using GoldEx.Sdk.Server.Infrastructure.Specifications;
+using GoldEx.Server.Domain.InvoiceAggregate;
+using GoldEx.Server.Domain.PaymentVoucherAggregate;
+
+namespace GoldEx.Server.Infrastructure.Specifications.Invoices;
+
+public class InvoicesByVoucherIdSpecification : SpecificationBase<Invoice>
+{
+    public InvoicesByVoucherIdSpecification(PaymentVoucherId paymentVoucherId)
+    {
+        AddCriteria(x => x.InvoicePayments.Any(ip => ip.PaymentVoucherId == paymentVoucherId));
+    }
+}
