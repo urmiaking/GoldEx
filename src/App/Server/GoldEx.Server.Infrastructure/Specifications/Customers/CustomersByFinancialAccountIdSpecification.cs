d89adb863735343c1ac443f2ed08512a372810ea
Masoud Khodadadi <masoud.xpress@gmail.com>
2025-08-06 21:03:49 +0330

    Added ledger account creation in payment voucher creation

---
+++ src/App/Server/GoldEx.Server.Infrastructure/Specifications/Customers/CustomersByFinancialAccountIdSpecification.cs
@@ -0,0 +1,15 @@
+﻿using GoldEx.Sdk.Server.Infrastructure.Specifications;
+using GoldEx.Server.Domain.CustomerAggregate;
+using GoldEx.Server.Domain.FinancialAccountAggregate;
+
+namespace GoldEx.Server.Infrastructure.Specifications.Customers;
+
+public class CustomersByFinancialAccountIdSpecification : SpecificationBase<Customer>
+{
+    public CustomersByFinancialAccountIdSpecification(FinancialAccountId financialAccountId)
+    {
+        AddInclude(x => x.FinancialAccounts!);
+        AddCriteria(x => x.FinancialAccounts != null 
+                         && x.FinancialAccounts.Any(f => f.Id == financialAccountId));
+    }
+}
