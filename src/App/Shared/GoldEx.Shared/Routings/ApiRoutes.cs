e2bc230eb5c0789ca976f5f009349364c2c22519
Masoud Khodadadi <masoud.xpress@gmail.com>
2025-10-17 20:19:08 +0330

    Fixed customer remaining amount

--- src/App/Shared/GoldEx.Shared/Routings/ApiRoutes.cs
+++ src/App/Shared/GoldEx.Shared/Routings/ApiRoutes.cs
@@ -91,7 +91,7 @@ public static class ApiRoutes
     public class Transactions
     {
         public const string Base = "/api/Transactions";
-        public const string GetRemainingList = "{customerId}/remaining-list";
+        public const string GetRemainingList = "customer/{customerId}/remaining-list";
         //public const string GetList = ""; // GET /api/Transactions or /api/Transactions?customerId=123
     }
 
