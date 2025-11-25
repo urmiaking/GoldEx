f0c120c86e7fc924400980be69d3673b5ee090bf
Masoud Khodadadi <masoud.xpress@gmail.com>
2025-11-26 00:19:55 +0330

    Increase gallery name length

---
+++ src/App/Server/GoldEx.Server.Infrastructure/Migrations/20251125204811_IncreaseInstitutionNameLength.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GoldEx.Server.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class IncreaseInstitutionNameLength : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "InstitutionName",
+                table: "Settings",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(25)",
+                oldMaxLength: 25);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "InstitutionName",
+                table: "Settings",
+                type: "nvarchar(25)",
+                maxLength: 25,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50);
+        }
+    }
+}
