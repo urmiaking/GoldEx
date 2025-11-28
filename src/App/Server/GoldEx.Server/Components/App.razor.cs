709e4d863eb3cc868319a31a4fbde7f471188862
Masoud Khodadadi <masoud.xpress@gmail.com>
2025-11-28 20:46:30 +0330

    Make the gallery name visible in loading card

--- src/App/Server/GoldEx.Server/Components/App.razor.cs
+++ src/App/Server/GoldEx.Server/Components/App.razor.cs
@@ -1,10 +1,13 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using GoldEx.Shared.Services.Abstractions;
+using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 
 namespace GoldEx.Server.Components;
 
 public partial class App
 {
+    private string? _institutionName;
+    [Inject] private ISettingService SettingService { get; set; } = default!;
     [CascadingParameter] private HttpContext HttpContext { get; set; } = default!;
 
     private IComponentRenderMode? RenderModeForPage => HttpContext.Request.Path.StartsWithSegments("/Account")
@@ -17,5 +20,17 @@ public partial class App
             return RenderMode.InteractiveServer;
 
         return new InteractiveAutoRenderMode(true);
+    }
+
+    protected override async Task OnInitializedAsync()
+    {
+        await LoadSettingsAsync();
+        await base.OnInitializedAsync();
+    }
+
+    private async Task LoadSettingsAsync()
+    {
+        var settings = await SettingService.GetAsync(CancellationToken.None);
+        _institutionName = settings?.InstitutionName;
     }
 }
