204513ede5e5176315b8ffe95c8c08213a550e5a
Masoud Khodadadi <masoud.xpress@gmail.com>
2025-12-10 12:39:58 +0330

    Improve inventory stock loading performance

--- src/App/Server/GoldEx.Server.Domain/PriceUnitAggregate/PriceUnit.cs
+++ src/App/Server/GoldEx.Server.Domain/PriceUnitAggregate/PriceUnit.cs
@@ -1,4 +1,5 @@
 ﻿using GoldEx.Sdk.Server.Domain.Entities;
+using GoldEx.Server.Domain.InventoryStockAggregate;
 using GoldEx.Server.Domain.PriceAggregate;
 using GoldEx.Shared.Enums;
 
@@ -35,6 +36,8 @@ public class PriceUnit : EntityBase<PriceUnitId>
 
     public PriceId? PriceId { get; private set; }
     public Price? Price { get; private set; }
+
+    public IReadOnlyList<InventoryStock>? InventoryStocks { get; private set; }
 
     public void SetTitle(string title) => Title = title;
     public void SetPriceId(PriceId? priceId) => PriceId = priceId;
