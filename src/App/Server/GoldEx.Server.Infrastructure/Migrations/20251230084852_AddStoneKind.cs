2778c6355b804ecf0245fa2b7f38e7949a5753e7
Masoud Khodadadi <masoud.xpress@gmail.com>
2025-12-30 12:20:05 +0330

    Add stone kind

---
+++ src/App/Server/GoldEx.Server.Infrastructure/Migrations/20251230084852_AddStoneKind.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GoldEx.Server.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddStoneKind : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "StoneTypes",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    EnTitle = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Symbol = table.Column<string>(type: "nvarchar(4)", maxLength: 4, nullable: false),
+                    Kind = table.Column<int>(type: "int", nullable: false),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StoneTypes", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StoneTypes_EnTitle",
+                table: "StoneTypes",
+                column: "EnTitle",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StoneTypes_Symbol",
+                table: "StoneTypes",
+                column: "Symbol",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StoneTypes_Title",
+                table: "StoneTypes",
+                column: "Title",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "StoneTypes");
+        }
+    }
+}
---
+++ src/App/Server/GoldEx.Server.Infrastructure/Repositories/Abstractions/IStoneTypeRepository.cs
@@ -0,0 +1,9 @@
+﻿using GoldEx.Sdk.Server.Infrastructure.Repositories;
+using GoldEx.Server.Domain.StoneTypeAggregate;
+
+namespace GoldEx.Server.Infrastructure.Repositories.Abstractions;
+
+public interface IStoneTypeRepository : IRepository<StoneType>,
+    ICreateRepository<StoneType>,
+    IUpdateRepository<StoneType>,
+    IDeleteRepository<StoneType>;
