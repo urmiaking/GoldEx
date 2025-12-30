8240700ba510a555bc8cfa9ab68e3180f15d4b93
Masoud Khodadadi <masoud.xpress@gmail.com>
2025-12-30 12:20:05 +0330

    Add stone kind

---
+++ src/App/Server/GoldEx.Server.Domain/StoneTypeAggregate/StoneType.cs
@@ -0,0 +1,39 @@
+﻿using GoldEx.Sdk.Server.Domain.Entities;
+using GoldEx.Shared.Enums;
+
+namespace GoldEx.Server.Domain.StoneTypeAggregate;
+
+public readonly record struct StoneTypeId(Guid Value);
+public class StoneType : EntityBase<StoneTypeId>
+{
+    public string Title { get; private set; }
+    public string EnTitle { get; private set; }
+    public string Symbol { get; private set; }
+    public StoneKind Kind { get; private set; }
+
+    public bool IsActive { get; private set; }
+
+#pragma warning disable CS8618
+    private StoneType() { }
+#pragma warning restore CS8618
+
+    private StoneType(string title, string enTitle, string symbol)
+    {
+        Id = new StoneTypeId(Guid.NewGuid());
+        Title = title;
+        EnTitle = enTitle;
+        Symbol = symbol;
+        IsActive = true;
+    }
+
+    public static StoneType Create(string title, string enTitle, string symbol) => new(title, enTitle, symbol);
+
+    public void Update(string title, string enTitle, string symbol)
+    {
+        Title = title;
+        EnTitle = enTitle;
+        Symbol = symbol;
+    }
+
+    public void SetActive(bool isActive) => IsActive = isActive;
+}
---
+++ src/App/Shared/GoldEx.Shared/DTOs/StoneTypes/StoneTypeRequestFilter.cs
@@ -0,0 +1,5 @@
+﻿using GoldEx.Shared.Enums;
+
+namespace GoldEx.Shared.DTOs.StoneTypes;
+
+public record StoneTypeRequestFilter(bool? IsActive, StoneKind? StoneKind);
