3b34151dc30d9297ab0b8d414869199b6c0592d4
Masoud Khodadadi <masoud.xpress@gmail.com>
2026-01-26 20:32:57 +0330

    fix gitignore

---
+++ src/App/Server/GoldEx.Server.Application/Validators/Backups/RestoreDatabaseValidator.cs
@@ -0,0 +1,32 @@
+﻿using FluentValidation;
+using GoldEx.Sdk.Common.DependencyInjections;
+using GoldEx.Server.Infrastructure.Repositories.Abstractions;
+
+namespace GoldEx.Server.Application.Validators.Backups;
+
+[ScopedService]
+internal class RestoreDatabaseValidator : AbstractValidator<string>
+{
+    private readonly IBackupRepository _repository;
+    public RestoreDatabaseValidator(IBackupRepository repository)
+    {
+        _repository = repository;
+
+        RuleFor(x => x)
+            .MustAsync(BeValidBackupAsync)
+            .WithMessage("فایل پشتیبان معتبر نمی باشد");
+    }
+
+    private async Task<bool> BeValidBackupAsync(string backupPath, CancellationToken cancellationToken = default)
+    {
+        try
+        {
+            await _repository.ValidateBackupAsync(backupPath, cancellationToken);
+            return true;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+}
