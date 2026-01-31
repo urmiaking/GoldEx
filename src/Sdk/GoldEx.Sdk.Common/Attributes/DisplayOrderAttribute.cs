07810b4fe32504587c4d0759b6d101f4fd5470b6
Masoud Khodadadi <masoud.xpress@gmail.com>
2026-01-31 10:43:07 +0330

    Fixed some styling issues

---
+++ src/Sdk/GoldEx.Sdk.Common/Attributes/DisplayOrderAttribute.cs
@@ -0,0 +1,7 @@
+﻿namespace GoldEx.Sdk.Common.Attributes;
+
+[AttributeUsage(AttributeTargets.Field)]
+public sealed class DisplayOrderAttribute(int order) : Attribute
+{
+    public int Order { get; } = order;
+}
