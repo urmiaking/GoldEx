1f2918df3ee5832618ccece08e459af70277e36f
Masoud Khodadadi <masoud.xpress@gmail.com>
2026-02-21 03:33:41 +0330

    Wire up server services and added simple calculator

---
+++ src/Calculator/Server/GoldEx.Calculator.Server/Extensions/WebHostingExtensions.cs
@@ -0,0 +1,75 @@
+﻿using GoldEx.Calculator.Client;
+using GoldEx.Calculator.Server.Common.Middlewares;
+using GoldEx.Calculator.Server.Components;
+using GoldEx.Server.Application;
+using GoldEx.Server.Infrastructure;
+using System.Reflection;
+
+namespace GoldEx.Calculator.Server.Extensions;
+
+public static class WebHostingExtensions
+{
+    public static WebApplication ConfigureServices(this WebApplicationBuilder builder)
+    {
+        var configuration = builder.Configuration;
+
+        builder.Services
+            .AddClientServerServices()
+            .AddServer(configuration)
+            .AddApplication()
+            .AddInfrastructure(configuration);
+
+        return builder.Build();
+    }
+
+    public static WebApplication ConfigurePipeline(this WebApplication app)
+    {
+        Assembly[] additionalAssemblies = [typeof(Routes).Assembly];
+
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseWebAssemblyDebugging();
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+        else
+        {
+            app.UseExceptionHandler("/Error", createScopeForErrors: true);
+            app.UseHsts();
+        }
+
+        app.Use((context, next) =>
+        {
+            context.Response.Headers["Cross-Origin-Embedder-Policy"] = "require-corp";
+            context.Response.Headers["Cross-Origin-Opener-Policy"] = "same-origin";
+            return next.Invoke();
+        });
+
+        app.UseMiddleware<ExceptionHandlerMiddleware>();
+
+        app.UseHttpsRedirection();
+
+        var cacheMaxAgeOneWeek = (60 * 60 * 24 * 7).ToString();
+        app.UseStaticFiles(new StaticFileOptions
+        {
+            OnPrepareResponse = ctx =>
+            {
+                ctx.Context.Response.Headers.Append("Cache-Control", $"public, max-age={cacheMaxAgeOneWeek}");
+            }
+        });
+
+        app.UseRouting();
+
+        app.UseAntiforgery();
+
+        app.MapStaticAssets();
+
+        app.MapControllers();
+        app.MapRazorComponents<App>()
+            .AddInteractiveWebAssemblyRenderMode()
+            .AddInteractiveServerRenderMode()
+            .AddAdditionalAssemblies(additionalAssemblies);
+
+        return app;
+    }
+}
--- src/Calculator/Server/GoldEx.Calculator.Server/Services/ConfigureSecurityStampOptions.cs
+++ src/Calculator/Server/GoldEx.Calculator.Server/Services/ConfigureSecurityStampOptions.cs
@@ -1,18 +1,15 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Options;
 
-namespace GoldEx.Server.Services;
+namespace GoldEx.Calculator.Server.Services;
 
 public class ConfigureSecurityStampOptions
     : IConfigureOptions<SecurityStampValidatorOptions>
 {
     public void Configure(SecurityStampValidatorOptions options)
     {
-        options.ValidationInterval = TimeSpan.FromMinutes(10); // Default interval is 30 minutes
+        options.ValidationInterval = TimeSpan.FromMinutes(10);
 
-        // When refreshing the principal, ensure custom claims that
-        // might have been set with an external identity continue
-        // to flow through to this new one.
         options.OnRefreshingPrincipal = refreshingPrincipal =>
         {
             var newIdentity = refreshingPrincipal.NewPrincipal?.Identities.First();
@@ -20,8 +17,6 @@ public class ConfigureSecurityStampOptions
 
             if (currentIdentity is not null && newIdentity is not null)
             {
-                // Since this is refreshing an existing principal, we want to merge all claims.
-                // Only work with claims in current identity that are not already present in the new identity with the same Type and Value.
                 var currentClaimsNotInNewIdentity = currentIdentity.Claims.Where(c => !newIdentity.HasClaim(c.Type, c.Value));
 
                 foreach (var claim in currentClaimsNotInNewIdentity)
