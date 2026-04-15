46ff95ca0d944c7d32a2071eaed09032f3134c51
Masoud Khodadadi <masoud.xpress@gmail.com>
2026-04-15 12:50:29 +0330

    Remove customerId from transactions

---
+++ src/App/Server/GoldEx.Server.Infrastructure/Migrations/20260415091901_RemoveCustomerId.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GoldEx.Server.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveCustomerId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Transactions_Customers_CustomerId",
+                table: "Transactions");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Transactions_CustomerId",
+                table: "Transactions");
+
+            migrationBuilder.DropColumn(
+                name: "CustomerId",
+                table: "Transactions");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "CustomerId",
+                table: "Transactions",
+                type: "uniqueidentifier",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Transactions_CustomerId",
+                table: "Transactions",
+                column: "CustomerId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Transactions_Customers_CustomerId",
+                table: "Transactions",
+                column: "CustomerId",
+                principalTable: "Customers",
+                principalColumn: "Id");
+        }
+    }
+}
