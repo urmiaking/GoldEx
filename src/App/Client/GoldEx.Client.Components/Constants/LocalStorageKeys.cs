a97a89324cd8d832c0ee643964aa2f3fb0d08e55
Masoud Khodadadi <masoud.xpress@gmail.com>
2026-05-07 08:50:37 +0330

    Fix passkey issue

--- src/App/Client/GoldEx.Client.Components/Constants/LocalStorageKeys.cs
+++ src/App/Client/GoldEx.Client.Components/Constants/LocalStorageKeys.cs
@@ -5,6 +5,7 @@ public static class LocalStorageKeys
     public const string IsDarkMode = "IsDarkMode";
     public const string SelectedPalette = "SelectedPalette";
     public const string LastSeenReleaseVersion = "LastSeenReleaseVersion";
+    public const string DeviceRegisteredPasskey = "DeviceRegisteredPasskey";
 
     public const string QuickInvoiceCompanyInfo = "QuickInvoiceCompanyInfo";
     public const string QuickInvoiceBasket = "QuickInvoiceBasket";
